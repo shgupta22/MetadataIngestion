5913d05e0adb0b07ef6986167eee898d42c4de83
vikash singh <singhvikash301094@gmail.com>
2018-05-31 18:02:41 +0530

    added MetadataConfigService

--- metadata-api/src/main/java/com/hashmapinc/metadata/config/service/MetadataConfigService.java
+++ metadata-api/src/main/java/com/hashmapinc/metadata/config/service/MetadataConfigService.java
@@ -1,4 +1,14 @@
 package com.hashmapinc.metadata.config.service;
 
-public interface MetadataConfigService {
+import com.hashmapinc.metadata.config.model.MetadataConfig;
+import com.hashmapinc.metadata.config.model.MetadataConfigId;
+
+public interface MetadataConfigService  {
+
+    MetadataConfig findMetadataConfigById(MetadataConfigId metadataConfigId);
+
+    MetadataConfig saveMetadataConfig(MetadataConfig metadataConfig);
+
+    void deleteMetadataConfig(MetadataConfigId metadataConfigId);
+
 }
