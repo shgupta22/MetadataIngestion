c7ff4e2e497faba696a74b7ac5c68b68a70e3e67
shubhamkumargupta <shubhamkumargupta09@gmail.com>
2018-05-31 18:30:04 +0530

    adding installation service and schema

--- metadata-api/src/main/java/com/hashmapinc/metadata/config/MetadataConfigApplication.java
+++ metadata-api/src/main/java/com/hashmapinc/metadata/config/MetadataConfigApplication.java
@@ -1,29 +1,33 @@
 package com.hashmapinc.metadata.config;
 
-//import com.hashmapinc.metadata.config.install.MetadataServiceInstall;
+import com.hashmapinc.metadata.config.install.MetadataServiceInstall;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 import java.util.Arrays;
-
 
 @EnableAutoConfiguration
 @SpringBootApplication
 @ComponentScan
+@Configuration
+@PropertySource("classpath:metadata-api.yml")
 public class MetadataConfigApplication {
+
     private static String SPRING_CONFIG_NAME_KEY = "--spring.config.name";
     private static String DEFAULT_SPRING_CONFIG_PARAM = SPRING_CONFIG_NAME_KEY + "=" + "metadata-api";
 
     public static void main(String[] args) {
-//        SpringApplication application = new SpringApplication(MetadataConfigApplication.class);
-//        ConfigurableApplicationContext context = application.run(updateArguments(args));
-//
-//        context.getBean(MetadataServiceInstall.class).performInstall();
-        SpringApplication.run(MetadataConfigApplication.class, args);
+        SpringApplication application = new SpringApplication(MetadataConfigApplication.class);
+        ConfigurableApplicationContext context = application.run(updateArguments(args));
+
+        context.getBean(MetadataServiceInstall.class).performInstall();
+//        SpringApplication.run(MetadataConfigApplication.class, args);
     }
 
     private static String[] updateArguments(String[] args) {
