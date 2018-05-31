e8496205f123a4b48760c7cd99e8252e51bfadbf
vikash singh <singhvikash301094@gmail.com>
2018-05-31 18:01:47 +0530

    Added missing @Component in MetadataConfigDao

--- metadata-api/src/main/java/com/hashmapinc/metadata/config/dao/MetadataConfigDaoImpl.java
+++ metadata-api/src/main/java/com/hashmapinc/metadata/config/dao/MetadataConfigDaoImpl.java
@@ -3,6 +3,7 @@ package com.hashmapinc.metadata.config.dao;
 import com.hashmapinc.metadata.config.entity.MetadataConfigEntity;
 import com.hashmapinc.metadata.config.model.MetadataConfig;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
@@ -13,6 +14,7 @@ import static com.hashmapinc.metadata.core.util.UUIDConverter.fromTimeUUID;
 
 //TODO: write the Dao utils having generic methods
 //TODO: See @Transactional functionality
+@Component
 public class MetadataConfigDaoImpl implements MetadataConfigDao {
 
     @Autowired
